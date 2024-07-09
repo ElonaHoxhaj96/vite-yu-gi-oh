46a557e6d565c8cac77f1d93e4c6d93cdc1e73ae
ElonaHoxhaj96 <elona6915@gmail.com>
2024-07-09 17:24:27 +0200

    feat: AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default{
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header>
+        <div id="logo"></div>
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
+</template>
+
+
+<style lang="scss">
+
+</style>
